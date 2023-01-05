0b9e6c7648fb077e8dd494dbf67a27ffff553e52
N0E-P <pecci.noe@gmail.com>
2023-01-05 20:32:11 +0100

    title, cool button & tailwind working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,55 @@
-import type {NextPage} from 'next'
-import {signIn, signOut, useSession} from 'next-auth/react'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 
 const Home: NextPage = () => {
-  const {data: session} = useSession()
+	const { data: session } = useSession();
 
-  if (session) {
-    const {user} = session
+	if (session) {
+		const { user } = session;
+		return (
+			<div>
+				<div className="flex m-auto bg-sky-300 text-center justify-center items-center h-full">
+					<h1 className="mx-40 text-4xl text-center font-bold items-center  my-4">
+						The Blockchain Adapter
+					</h1>
+					<button
+						className="bg-sky-600 p-3 mr-5 my-3 rounded-3xl text-white hover:scale-105 ease-in duration-300"
+						onClick={() => signOut()}
+					>
+						<div className="flex justify-center items-center text-center">
+							{user?.image && (
+								<Image
+									src={user.image}
+									alt=""
+									width={38}
+									height={38}
+									style={{ borderRadius: "50%" }}
+								/>
+							)}
+							<div className="ml-2">{user?.name}</div>
+						</div>
+					</button>
+				</div>
+			</div>
+		);
+	}
 
-    return (
-      <>
-        {user?.image && (
-          <Image
-            src={user.image}
-            alt=""
-            width={38}
-            height={38}
-            style={{borderRadius: '50%'}}
-          />
-        )}
-        Hello, {user?.name}!<br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
+	return (
+		<div>
+			<div className="flex m-auto bg-sky-300 text-center justify-center items-center h-full">
+				<h1 className="mx-40 text-4xl text-center font-bold items-center  my-4">
+					The Blockchain Adapter
+				</h1>
+				<button
+					className="bg-sky-600 p-3 rounded-3xl text-white hover:scale-105 ease-in duration-300"
+					onClick={() => signIn()}
+				>
+					Sign in
+				</button>
+			</div>
+		</div>
+	);
+};
 
-  return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  )
-}
-
-export default Home
+export default Home;
