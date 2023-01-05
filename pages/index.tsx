58707fe5d27d342fde28415d8eeb69ac7c98a024
N0E-P <pecci.noe@gmail.com>
2023-01-05 14:35:24 +0100

    website initialisation

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>The Blockchain Adapter</title>
+				<meta name="description" content="" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main>
+				<h1 className="text-5xl text-center">The Blockchain Adapter</h1>
+			</main>
+		</>
+	);
+}
