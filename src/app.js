000238fc415563e524a72010d1c1e758912faa8b
N0E-P <pecci.noe@gmail.com>
2023-01-08 13:13:23 +0100

    WORKING new version with express.js

---
+++ src/app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+const session = require("express-session");
+const passport = require("passport");
+const DiscordStrategy = require("./strategies/discordstrategy");
+
+// Routes
+const authRoute = require("./routes/auth");
+
+app.use(
+	session({
+		secret: "i eat pasta",
+		cookie: {
+			maxAge: 60000 * 60 * 24,
+		},
+		saveUninitialized: false,
+	})
+);
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Middlewares Routes
+app.use("/auth", authRoute);
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
