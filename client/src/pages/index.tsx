e0835e00315cf5ec32d81968082882d1883ca40f
N0E-P <pecci.noe@gmail.com>
2023-01-09 17:59:37 +0100

    init of nextjs app

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>The Blockchain Adapter</title>
+				<meta name="description" content="The Blockchain Adapter" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main>
+				<p>Home page</p>
+				<button></button>
+			</main>
+		</>
+	);
+}
