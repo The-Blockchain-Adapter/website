ffacff2e91e2024fd9094377913c46b4188a79c6
N0E-P <pecci.noe@gmail.com>
2023-01-11 17:27:50 +0100

    CA MARCHEEEE !!!!! YESSSSSSSSSSS

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,13 +1,12 @@
 import { GuildItem } from "../../components/GuildItem";
-import { useRouter } from "next/router";
-import { getSession, useSession } from "next-auth/react";
+import { getSession } from "next-auth/react";
 
 // @ts-ignore
-export default async function DashboardPage() {
-	//GET ALL THE USER GUILDS
+export default function DashboardPage() {
 	return (
 		<div>
 			<h1>Select a Guild</h1>
+
 			{/*
 				// @ts-ignore
 				guilds.map((guild) => (
@@ -21,8 +20,8 @@ export default async function DashboardPage() {
 
 //@ts-ignore
 export async function getServerSideProps(context) {
+	// Check if the user is connected. Otherwise return him to the home page
 	const session = await getSession(context);
-	/*
 	if (!session) {
 		return {
 			redirect: {
@@ -31,13 +30,36 @@ export async function getServerSideProps(context) {
 			},
 		};
 	}
-	/*
-	const me = await fetch("http://discord.com/api/users/@me", {
+
+	const userGuilds = await fetch("http://discord.com/api/users/@me/guilds", {
 		// @ts-ignore
 		headers: { Authorization: `Bearer ${session.accessToken}` },
 	}).then((res) => res.json());
+
+	//trier pour ne retourner que les guilds a afficher
+	const adminUserGuilds = userGuilds.filter(
+		// @ts-ignore
+		({ permissions }) => (parseInt(permissions) & 0x8) === 0x8
+	);
+
+	console.log(adminUserGuilds);
+
+	/*
+	const botGuilds = await fetch("http://discord.com/api/v9/users/@me/guilds", {
+		// @ts-ignore
+		headers: { Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}` },
+	}).then((res) => res.json());
+	console.log(botGuilds);
+
+
+	
+
+	// @ts-ignore
+	const mutualAdminGuilds = adminUserGuilds.filter((guild) =>
+		botGuilds.some((botGuild) => botGuild.id === guild.id)
+	);
 */
 	return {
-		props: { session },
+		props: { adminUserGuilds },
 	};
 }
