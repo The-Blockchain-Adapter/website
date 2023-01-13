693ed7de82a6ca66ade3cd7e240c403ea9ccdb86
N0E-P <pecci.noe@gmail.com>
2023-01-13 09:26:40 +0100

    WORKING JAVASCRIPT VERSION

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,8 @@
 import { SessionProvider } from "next-auth/react";
-import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import { Header } from "../components/Header";
 
-export default function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+export default function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 	return (
 		<SessionProvider session={session}>
 			<Header />
