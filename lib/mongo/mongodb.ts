cc558abc4b203b5e5c792c28d5510faef9867c86
N0E-P <pecci.noe@gmail.com>
2023-01-13 08:33:18 +0100

    .

--- lib/mongo/mongodb.ts
+++ lib/mongo/mongodb.ts
@@ -11,7 +11,9 @@ if (!uri) {
 let client;
 let clientPromise: Promise<MongoClient>;
 
-if (process.env.NODE_ENV === "development") {
+const NODE = process.env.NODE_ENV || "live";
+
+if (NODE === "development") {
 	// In development mode, use a global variable so that the value
 	// is preserved across module reloads caused by HMR (Hot Module Replacement).
 	if (!global._mongoClientPromise) {
