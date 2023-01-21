49fa25722bdfd53e7c4f9920f70638197c2546f5
N0E-P <pecci.noe@gmail.com>
2023-01-21 19:19:54 +0100

    guild header always on same line

--- components/GuildHeader.jsx
+++ components/GuildHeader.jsx
@@ -14,7 +14,7 @@ export const GuildHeader = ({ guild }) => {
 	}
 
 	return (
-		<main className="sm:flex items-center max-w-[1200px]">
+		<main className="flex items-center max-w-[1200px]">
 			<button
 				onClick={() => router.push(`/dashboard/${router.query.id}`)}
 				className="text-[#1f2937] mr-0 sm:mr-4 m-4 bg-gray-300 rounded-full flex items-center hover:scale-100 shadow-md shadow-gray-400"
@@ -24,10 +24,10 @@ export const GuildHeader = ({ guild }) => {
 			</button>
 			<button
 				onClick={() => router.push(`/dashboard/${router.query.id}/create`)}
-				className="flex justify-center items-center rounded-full duration-300"
+				className="sm:p-3 p-2 flex justify-center items-center rounded-full duration-300"
 			>
-				<AiFillPlusCircle size={35} />
-				<div className="mx-2">Create a new script </div>
+				<AiFillPlusCircle size={45} />
+				<h4 className="mx-2 hidden sm:flex text-base sm:text-lg">Create a new script </h4>
 			</button>
 		</main>
 	);
